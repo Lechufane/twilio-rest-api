2e147ee14c74ea1fa6ede4168590e43382a2b97a
Lechufane <diego.villafane94@gmail.com>
2022-08-19 16:33:17 -0300

    Finished endpoints for sending whatsapp, get conversations, get conversation by participant, and get call logs by number

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -11,60 +11,106 @@ const TWILIO_NUMBER = process.env.NUMERO_CHILE;
 router.post('/sendMessage', async (req, res) => {
 
     let convSid;
+    let participantSid;
+    let messageSid;
 
-    let createConversation = await client.conversations.conversations
-    .create({friendlyName: 'SMS-to-WhatsApp Example'})
+    try {
+    let createConversation = await client.conversations.v1.conversations
+    .create({friendlyName: 'numero-de-cliente'})
     .then(conversation => {
         convSid = conversation.sid;
-        res.send(conversation.sid)
+        console.log(convSid);
     });
+    } catch (error) {
+        console.error('conv Error: ', error);
+    }
 
-    let addParticipantWspp = await client.conversations.conversations(convSid)
+    try {
+    let addParticipantWspp = await client.conversations.v1.conversations(convSid)
     .participants.create({
-        'messagingBinding.address': 'whatsapp:+5492616138625',
-         'messagingBinding.proxyAddress': `whatsapp:${TWILIO_NUMBER}`
+        'messagingBinding.address': 'whatsapp:+5492622408898',
+        'messagingBinding.proxyAddress': `whatsapp:+56227127123}`
         })
-
-    let addParticipant = await client.conversations.conversations(convSid)
-    .participants.create({
-        'messagingBinding.address': '+5492616138625',
-        'messagingBinding.proxyAddress': `${TWILIO_NUMBER}`
+        .then(participant => {
+            participantSid = participant.sid;
         })
+    } catch (error) {
+        console.error('participant Error: ', error);
+    }
+
+    try {
+    let messageSent = client.conversations.v1.conversations(convSid)
+    .messages
+    .create({
+       author:`whatsapp:${TWILIO_NUMBER}`,
+       body: 'Hola Bienvenido a QuePlan.cl'
+     })
+    .then(message => messageSid = message.sid);
+
+    } catch (error) {
+        console.error('message Error: ', error);
+    }
+    res.send('Message sent');
+    }
+);
+
+
+
+router.get('/conversations', async(req, res) => {
+    
+    try {
+    const conversationList = await client.conversations.v1.conversations.list({limit: 20});
+    const conversationsSid = conversationList.map(({sid}) => sid)
+    const messagesList = await Promise.all(conversationsSid.map(conversation => client.conversations.v1.conversations(conversation).messages.list({limit:20})))
+    const messagesArray = messagesList.map(messages => messages.map(({body}) => body))
+    res.send(messagesArray); 
+
+    } catch (error) {
+        console.error('conversation Error: ', error);
+    }
 
 })
 
-router.get('/conversations', (req, res) => {
-    let conversationsArray = []
-    client
-    .conversations
-    .conversations
-    .list({limit: 20})
-    .then(conversations => conversations.forEach(c => conversationsArray.push(c)))
-    .then(() => res.send(conversationsArray))
-    .catch(err => console.error(err))
-})
+router.get('/conversationsByParticipant', async(req, res) => {
 
-router.get('/conversationByParticipant', (req, res) => {
 
-    let conversationsArray = []
-    client
-    .conversations
-    .participantConversations
-    .list({address: '+5492616138635', limit: 2})
-    .then(participantConversations => {
-        participantConversations.forEach(p => conversationsArray.push(p))})
-    .then(() => res.send(conversationsArray))
-    .catch(err => console.error(err))
+    try{
+    let participantConversationSid = await client.conversations.v1.participantConversations.list({address: 'whatsapp:+5492616138635', limit: 5})
+    let participantConversationSidArray = participantConversationSid.map(({conversationSid}) => conversationSid)
+    let messagesList = await Promise.all(participantConversationSidArray.map(conversation => client.conversations.v1.conversations(conversation).messages.list({limit:10})))
+    let messagesArray = messagesList.map(messages => messages.map(({body}) => body))
+    res.send(messagesArray);
+    } catch (error) {
+        console.error('conversation Error: ', error);
+    }
 
 })
 
-router.get('/fetchConversatParticipant', (req, res) => {
-    client.conversations.v1.conversations('CH00199b11afd1420d917df21739227a4f')
-      .participants('MBd221e13d3ee547cdbbebe3b40ff9591f')
-      .fetch()
-      .then(participant => res.send(participant));
+router.get('/conversationsByParticipant', async(req, res) => {
+
+
+    try{
+    let participantConversationSid = await client.conversations.v1.participantConversations.list({address: 'whatsapp:+5492616138635', limit: 5})
+    let participantConversationSidArray = participantConversationSid.map(({conversationSid}) => conversationSid)
+    let messagesList = await Promise.all(participantConversationSidArray.map(conversation => client.conversations.v1.conversations(conversation).messages.list({limit:10})))
+    let messagesArray = messagesList.map(messages => messages.map(({body}) => body))
+    res.send(messagesArray);
+    } catch (error) {
+        console.error('conversation Error: ', error);
+    }
+
 })
 
+router.get('/getCallsFromNumber', async (req, res) => {
+
+    let clientCallsLog = await client.insights.v1.callSummaries.list({to: '+56227127123', limit: 20})
+    let clientCallsLogArray = clientCallsLog.map(({from, startTime, endTime, duration, callSid}) => {
+        return {from, startTime, endTime, duration, callSid}
+    })
+    console.log(clientCallsLogArray);
+
+});
+    
 
 
 module.exports = router;
